cdb6a6ad7297e9e11ba05939ec0a017a08774237
Rob Ross <djrobx@hotmail.com>
2016-09-27 08:05:31 -0700

    Adding some validation to int32 near jumps to see if they're related to Toxie's crashing problem

--- src/windows/jitemit.c
+++ src/windows/jitemit.c
@@ -78,6 +78,7 @@
 #include <string.h>
 #include <assert.h>
 #include <stdint.h>
+#include <windows.h>
 
 #include "memory.h"
 #include "jit.h"
@@ -722,7 +723,13 @@ void jit_emit_commit(struct jit_ctl *jit)
 
 				// get the label address
 			    byte *lblnat = label_to_native(jit, i->lbl);
-				int lbldelta = lblnat - (i->nataddr + i->len);
+				
+				//int lbldelta = lblnat - (i->nataddr + i->len);
+				INT64 lbldelta = (INT64)lblnat - ((INT64)i->nataddr + (INT64)i->len);
+				assert(lbldelta >= INT32_MIN && lbldelta <= INT32_MAX);
+				if(!(lbldelta >= INT32_MIN && lbldelta <= INT32_MAX))
+					MessageBoxW(NULL, L"Jump span overflow", L"Code is too far away", MB_OK);
+
 
 				// If it's one of the special emulator handlers, it will definitely be
 				// out of range of a one-byte jump.  Further, we must use a proxy jump,
@@ -928,7 +935,12 @@ void jit_emit_commit(struct jit_ctl *jit)
 			// get the native code address of the label target, and
 			// calculate the offset from the end of this instruction
 			byte *lblnat = label_to_native(jit, i->lbl);
-			int lbldelta = lblnat - (i->nataddr + i->len);
+//			int lbldelta = lblnat - (i->nataddr + i->len);
+			INT64 lbldelta = (INT64)lblnat - ((INT64)i->nataddr + (INT64)i->len);
+			assert(lbldelta >= INT32_MIN && lbldelta <= INT32_MAX);
+			// Temporary checking toxies problem
+			if(!(lbldelta >= INT32_MIN && lbldelta <= INT32_MAX))
+				MessageBoxW(NULL, L"Jump span overflow", L"Code is too far away", MB_OK);
 
 			// There can be no unresolved labels remaining at this point.
 			// Anything that was pointing to non-translated code must have
